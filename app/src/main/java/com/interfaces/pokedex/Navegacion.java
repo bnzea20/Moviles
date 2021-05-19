ffc11d0e2ae3e33e99958761c387c884b86ccd5d
maflorez14 <maflorez14@ucatolica.edu.co>
2021-05-19 02:59:48 -0500

    Se realizo la conexión con la API y el modulo de mostrar pokemones, nombre e imagen, faltan los demás atributos

--- app/src/main/java/com/interfaces/pokedex/Navegacion.java
+++ app/src/main/java/com/interfaces/pokedex/Navegacion.java
@@ -1,6 +1,7 @@
 package com.interfaces.pokedex;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 
@@ -15,10 +16,37 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import com.interfaces.pokedex.pokeAPI.PokeapiService;
+import com.interfaces.pokedex.pokeAPI.Pokemon;
+import com.interfaces.pokedex.pokeAPI.PokemonRespuesta;
+
+
+import java.util.ArrayList;
 
 public class Navegacion extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private static final String TAG = "POKEDEX";
+
+    private Retrofit retrofit;
+
+    private RecyclerView recyclerView;
+    private ListaPokemonAdapter listaPokemonAdapter;
+
+    private int offset;
+
+    private boolean aptoParaCargar;
+
+    //Se utiliza para consumir API y procesar la respuesta JSON obtenida
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +70,75 @@ public class Navegacion extends AppCompatActivity {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        listaPokemonAdapter = new ListaPokemonAdapter(this);
+        recyclerView.setAdapter(listaPokemonAdapter);
+        recyclerView.setHasFixedSize(true);
+        final GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (dy > 0) {
+                    int visibleItemCount = layoutManager.getChildCount();
+                    int totalItemCount = layoutManager.getItemCount();
+                    int pastVisibleItems = layoutManager.findFirstVisibleItemPosition();
+
+                    if (aptoParaCargar) {
+                        if ((visibleItemCount + pastVisibleItems) >= totalItemCount) {
+                            Log.i(TAG, " Llegamos al final.");
+
+                            aptoParaCargar = false;
+                            offset += 20;
+                            obtenerDatos(offset);
+                        }
+                    }
+                }
+            }
+        });
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://pokeapi.co/api/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        aptoParaCargar = true;
+        offset = 0;
+        obtenerDatos(offset);
+    }
+
+    private void obtenerDatos(int offset) {
+        PokeapiService service = retrofit.create(PokeapiService.class);
+        Call<PokemonRespuesta> pokemonRespuestaCall = service.obtenerListaPokemon(20, offset);
+
+        pokemonRespuestaCall.enqueue(new Callback<PokemonRespuesta>() {
+            @Override
+            public void onResponse(Call<PokemonRespuesta> call, Response<PokemonRespuesta> response) {
+                aptoParaCargar = true;
+                if (response.isSuccessful()) {
+
+                    PokemonRespuesta pokemonRespuesta = response.body();
+                    ArrayList<Pokemon> listaPokemon = pokemonRespuesta.getResults();
+
+                    listaPokemonAdapter.adicionarListaPokemon(listaPokemon);
+
+                } else {
+                    Log.e(TAG, " onResponse: " + response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PokemonRespuesta> call, Throwable t) {
+                aptoParaCargar = true;
+                Log.e(TAG, " onFailure: " + t.getMessage());
+            }
+        });
     }
 
     @Override
@@ -54,10 +148,10 @@ public class Navegacion extends AppCompatActivity {
         return true;
     }
 
-    @Override
+    /*@Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
+    }*/
 }
