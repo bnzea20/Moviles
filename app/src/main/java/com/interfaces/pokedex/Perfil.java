aabbdd8ace6d210f648ffe03d714acc30b69e6b7
Cristian Camilo Zambrano Lozada <63761110+criscamizambrano@users.noreply.github.com>
2021-06-16 14:41:50 -0500

    Se corrigen errores menores

--- app/src/main/java/com/interfaces/pokedex/Perfil.java
+++ app/src/main/java/com/interfaces/pokedex/Perfil.java
@@ -2,9 +2,15 @@ package com.interfaces.pokedex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -12,6 +18,8 @@ public class Perfil extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
     private TextView Txtcorreo, TxtUID;
+    private ImageView foto;
+    private FloatingActionButton botonFlotante;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +27,26 @@ public class Perfil extends AppCompatActivity {
         setContentView(R.layout.activity_perfil);
         Txtcorreo=(TextView)findViewById(R.id.txtCorreo);
         TxtUID=(TextView)findViewById(R.id.txtUID);
+        foto=(ImageView)findViewById(R.id.imageView4);
+        botonFlotante=(FloatingActionButton) findViewById(R.id.fab2);
         firebaseAuth=FirebaseAuth.getInstance();
         FirebaseUser user = firebaseAuth.getInstance().getCurrentUser();
 
         if(user!=null){
             Txtcorreo.setText("Correo: "+user.getEmail().toString().trim());
             TxtUID.setText("UID: "+user.getUid().toString().trim());
+            Glide.with(this).load(user.getPhotoUrl()).into(foto);
         }
+
+        botonFlotante.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(Perfil.this,"Se cerró la sesión", Toast.LENGTH_LONG).show();
+                Intent i = new Intent(Perfil.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
 
     }
 }
